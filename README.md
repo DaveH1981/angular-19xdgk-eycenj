6cc0cccda5ea1f682c3d7cbe310f30a01c1efc86
DaveH1981 <daveh1981@gmail.com>
2020-05-04 19:04:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-19xdgk-eycenj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-19xdgk-eycenj)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Ultimate Shredder',
+    price: 2499,
+    description: 'A beast of a guitar with everything you need for heavy riffage and fretboard pyrotechnics. The ultimate metal guitar!'
+  },
+  {
+    name: 'Wes Paul',
+    price: 2999,
+    description: 'The classic set-neck beauty with humbuckers. She sings for days with great sustain.'
+  },
+  {
+    name: 'Startercaster',
+    price: 299,
+    description: 'Perfect first guitar, modeled after the same model a certain famous Jimi played.'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
